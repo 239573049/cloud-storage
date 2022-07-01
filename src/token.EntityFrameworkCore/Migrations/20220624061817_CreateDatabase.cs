e03b6f24ed4a11ef655aa3be013cca77890870d6
token <hejiale@xymind.cn>
2022-07-01 17:18:21 +0800

    删除

---
+++ src/token.EntityFrameworkCore/Migrations/20220624061817_CreateDatabase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace token.EntityFrameworkCore.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AppVersion",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Code = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Version = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Download = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UpdateTime = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    UpdateContent = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ForcedUpdating = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppVersion", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    AccountNumber = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PhoneNumber = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    HeadPortrait = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppVersion_Id",
+                table: "AppVersion",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Id",
+                table: "Users",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppVersion");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- src/token.Web/Program.cs
+++ src/token.Web/Program.cs
@@ -29,42 +29,6 @@ builder.Host.AddAppSettingsSecretsJson()
     .UseAutofac()
     .UseSerilog();
 
-var consul = Environment.GetEnvironmentVariable("consul") ?? "http://124.222.27.83:8500";
-builder.Configuration
-    .AddConsul("token", options =>
-    {
-        options.Parser = new SimpleConfigurationParser();
-        options.ConsulConfigurationOptions = cco =>
-        {
-            cco.Address = new Uri(consul);
-            options.Optional = true;
-            options.ReloadOnChange = true;
-            options.OnLoadException = exception =>
-            {
-                Console.WriteLine(exception.Exception.Message);
-            };
-            options.ConvertConsulKVPairToConfig = kvPair =>
-            {
-                var normalizedKey = kvPair.Key
-                    .Replace("token/", string.Empty)
-                    .Replace("__", "/")
-                    .Replace("/", ":")
-                    .Trim('/');
-
-                using Stream valueStream = new MemoryStream(kvPair.Value);
-                using var streamReader = new StreamReader(valueStream);
-                var parsedValue = streamReader.ReadToEnd();
-
-                return new Dictionary<string, string>()
-                {
-                { normalizedKey, parsedValue }
-                };
-            };
-        };
-        options.ReloadOnChange = true;
-    }).Build();
-
-
 builder.Services.AddSingleton(Log.Logger);
 
 await builder.AddApplicationAsync<TokenWebModule>();
@@ -72,38 +36,4 @@ var app = builder.Build();
 await app.InitializeApplicationAsync();
 app.MapControllers();
 
-// 在环境变量获取当前服务的服务名称
-// var serviceName = Environment.GetEnvironmentVariable("ServiceName");
-// var servicePort = Environment.GetEnvironmentVariable("ServicePort");
-
-RegisterConsul(app, app.Configuration, app.Lifetime);
-
 await app.RunAsync();
-void RegisterConsul(IApplicationBuilder app, IConfiguration configuration, IHostApplicationLifetime lifetime)
-{
-    var consulClient = new ConsulClient(x =>
-    {
-        x.Address = new Uri(consul);
-    });
-
-    var registration = new AgentServiceRegistration()
-    {
-        ID = Guid.NewGuid().ToString(),
-        Name = "consulOption.ServiceName",
-        Address = "http://124.222.27.83:8000",
-        Port = 80,
-        Check = new AgentCheckRegistration()
-        {
-            DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(100),
-            Interval = TimeSpan.FromSeconds(100),
-            HTTP = "http://124.222.27.83:31005/health",
-            Timeout = TimeSpan.FromSeconds(100)
-        }
-    };
-
-    consulClient.Agent.ServiceRegister(registration).Wait();
-    lifetime.ApplicationStopping.Register(() =>
-    {
-        consulClient.Agent.ServiceDeregister(registration.ID).Wait();
-    });
-}
